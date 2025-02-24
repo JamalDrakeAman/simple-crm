cdb159fe6e768b546cd015dbd7276cb165546133
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-02-25 00:23:55 +0100

    dialog comp

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {
+  MatDialogContent,
+  MatDialogClose,
+  MatDialogActions
+} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  imports: [
+    MatDialogContent,
+    MatDialogClose,
+    MatDialogActions,
+    MatInputModule,
+    MatFormFieldModule
+  ],
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss'
+})
+export class DialogAddUserComponent {
+
+}

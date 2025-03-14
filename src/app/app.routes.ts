d54b3b833966592c7672dfe8c64b746fe923fa09
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-03-14 15:07:12 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { CustomersComponent } from './customers/customers.component';
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'user', component: UserComponent },
+    { path: 'customers', component: CustomersComponent },
     { path: 'user/:id', component: UserDetailComponent }
 ];

cf5180c2cc9521a7f3d748c41bf89d2f8d5b276a
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-03-16 19:38:25 +0100

    update clock style

--- src/app/dashboard/clock/clock.component.ts
+++ src/app/dashboard/clock/clock.component.ts
@@ -11,18 +11,30 @@ export class ClockComponent {
 
   clock = '00 : 00 : 00';
 
-  constructor(){
-    this.updateClock(); 
-    setInterval(() => this.updateClock(), 1000); 
+  hour = '';
+
+  minute = '';
+
+  second = '';
+
+
+  constructor() {
+    this.updateClock();
+    setInterval(() => this.updateClock(), 1000);
   }
 
-  updateClock(){
+  updateClock() {
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
     const timeString = `${hours} : ${minutes} : ${seconds}`
+
     this.clock = timeString;
+
+    this.hour = `${hours}`;
+    this.minute = `${minutes}`;
+    this.second = `${seconds}`;
   }
 
 }

33ac63cad84c624b08f3732284ee20d6d17bcdd3
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-03-17 12:07:46 +0100

    include mixins and keyframes

--- src/app/shared/services/theme.service.ts
+++ src/app/shared/services/theme.service.ts
@@ -6,26 +6,25 @@ import { Injectable, signal } from '@angular/core';
 export class ThemeService {
 
   isDarkMode = false;
-
-  constructor() { 
-    const savedMode = localStorage.getItem('darkMode');
-    this.isDarkMode = savedMode === 'true';
-  }
-
-  toggleDarkMode(checked: boolean): void {
-    this.isDarkMode = checked;
-    localStorage.setItem('darkMode', this.isDarkMode.toString());
-    console.log(this.isDarkMode);
-  }
-
   darkMode = signal(false);
 
-  toggle(){
+  constructor() {
+    const savedMode = localStorage.getItem('theme');
+    this.darkMode.set(savedMode === 'true')
+  }
+
+  toggle() {
     this.darkMode.set(!this.darkMode())
     localStorage.setItem('theme', JSON.stringify(this.darkMode()));
     console.log(this.darkMode());
   }
 
 
+
+  // toggleDarkMode(checked: boolean): void {
+  //   this.isDarkMode = checked;
+  //   localStorage.setItem('darkMode', this.isDarkMode.toString());
+  //   console.log(this.isDarkMode);
+  // }
 
 }

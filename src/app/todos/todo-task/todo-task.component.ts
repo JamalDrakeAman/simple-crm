82aabc57ef07ab4f1658a41e8957cd7f501fde0e
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-03-24 00:42:20 +0100

    update todo in firestore

--- src/app/todos/todo-task/todo-task.component.ts
+++ src/app/todos/todo-task/todo-task.component.ts
@@ -2,6 +2,10 @@ import { Component, inject, Input, AfterViewInit, ViewChild, ElementRef } from '
 import { ThemeService } from '../../shared/services/theme.service';
 import { CommonModule } from '@angular/common';
 
+
+import { FirestoreServiceService } from '../../shared/services/firestore-service.service';
+import { doc, setDoc } from "firebase/firestore";
+
 @Component({
   selector: 'app-todo-task',
   standalone: true,
@@ -13,9 +17,20 @@ import { CommonModule } from '@angular/common';
 })
 export class TodoTaskComponent {
 
+  todoTaskData = inject(FirestoreServiceService);
+
   theme = inject(ThemeService);
 
-  @Input() task!: { id: number, title: string, description: string, completed: boolean };
+  @Input() task!: {
+    id: number,
+    title: string,
+    description: string,
+    completed: boolean,
+    timestamp: Date,
+    priority: string
+
+
+  };
 
   @ViewChild('descriptionContainer', { static: false }) descriptionContainer!: ElementRef;
 
@@ -28,6 +43,11 @@ export class TodoTaskComponent {
 
   toggleTaskCompletion(task: any) {
     task.completed = !task.completed;
+    this.writeTaskData()
+
+    console.log('task', task);
+    console.log('task id', task.id);
+
   }
 
   ngAfterViewInit() {
@@ -40,4 +60,27 @@ export class TodoTaskComponent {
   }
 
 
+  async writeTaskData() {
+
+    // const db = getFirestore();
+
+    const db = this.todoTaskData.db
+
+    await setDoc(doc(db, "todos/" + this.task.id), {
+      title: this.task.title,
+      description: this.task.description,
+      priority: this.task.priority,
+      timestamp: this.task.timestamp,
+      completed: this.task.completed
+    });
+  }
+
+
 }
+
+
+// title: this.task.title,
+//   description: this.task.description,
+//     priority: this.task.priority,
+//       timestamp: this.task.timestamp,
+//         completed: this.task.completed
